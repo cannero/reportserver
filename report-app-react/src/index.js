286b40d50a43291df533b15951e274850b534e87
Cannero <serunmal@gmail.com>
2017-07-27 21:59:16 +0200

    initial react app does not work with handsontable

--- report-app-react/src/index.js
+++ report-app-react/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import moment from 'moment';
+import numbro from 'numbro';
+import pikaday from 'pikaday';
+//import Zeroclipboard from 'zeroclipboard';
+import Handsontable from 'handsontable/dist/handsontable.full';
+//import Handsontable from 'handsontable/dist/handsontable.full';
+import HotTable from 'react-handsontable/dist/react-handsontable';
+//import HotTable from 'react-handsontable/dist/react-handsontable'; 
+//import 'handsontable/dist/handsontable.full.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
