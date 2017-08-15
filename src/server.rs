7f6a3b20fe1f842a0f2f33051b19fc52ad2131a4
Cannero <serunmal@gmail.com>
2017-08-15 22:34:16 +0200

    query (form-urlencoded) parsing added to server

--- src/server.rs
+++ src/server.rs
@@ -3,6 +3,7 @@ extern crate router;
 extern crate staticfile;
 extern crate mount;
 extern crate time;
+extern crate url;
 
 use iron::prelude::*;
 use router::Router;
@@ -20,12 +21,23 @@ fn hello_world(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok, ["Hello World ", &path].concat())))
 }
 
+fn data_for_employees(req: &mut Request) -> IronResult<Response> {
+    let parsed_url: url::Url = (req.url.clone()).into();
+    
+    for key_value in  parsed_url.query_pairs().into_owned() {
+        println!("query is {:?}", key_value);
+    }
+    Ok(Response::with(iron::status::Ok))
+}
+
+
 fn main() {
 
-    let mut router = Router::new();
-    router.get("/:page", hello_world, "index");
+    let mut api_router = Router::new();
+    api_router.get("/:page", hello_world, "page");
+    api_router.get("/peremployee", data_for_employees, "per_employee");
 
-    let mut cors_chain = Chain::new(router);
+    let mut cors_chain = Chain::new(api_router);
     cors_chain.link_after(CorsMiddleware);
     
     let staticfiles = Static::new(Path::new("public"));
@@ -37,6 +49,8 @@ fn main() {
     chain.link_before(ConsoleResponder);
     chain.link_after(ConsoleResponder);
 
-    Iron::new(chain).http("localhost:3000").unwrap();
+    let main_url = "localhost:3000";
+    println!("starting server on: {}", main_url);
+    Iron::new(chain).http(main_url).unwrap();
 }
 
